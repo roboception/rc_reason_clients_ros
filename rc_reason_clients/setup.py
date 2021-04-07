366d8024c6e938d6e9ca262c51e9c416ead84e6c
Felix Ruess <felix.ruess@roboception.de>
2021-04-07 12:23:26 +0200

    as module
    figure out how to wrap to call with rosrun later..

--- rc_reason_clients/setup.py
+++ rc_reason_clients/setup.py
@@ -7,6 +7,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 setup_args = generate_distutils_setup(
     packages=['rc_reason_clients'],
     package_dir={'': 'src'},
+    requires=['rospy']
 )
 
 setup(**setup_args)
