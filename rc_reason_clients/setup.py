6deb52b0c0d3b6de370c105a6c2c2768d3f0b118
Felix Ruess <felix.ruess@roboception.de>
2021-04-13 14:53:44 +0200

    [ci] adapt layout and run on ci

--- rc_reason_clients/setup.py
+++ rc_reason_clients/setup.py
@@ -6,10 +6,14 @@ from catkin_pkg.python_setup import generate_distutils_setup
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
     packages=['rc_reason_clients'],
-    package_dir={'': 'src'},
+    package_dir={'': '.'},
     scripts=['scripts/rc_hand_eye_calibration_client',
              'scripts/rc_april_tag_detect_client',
-             'scripts/rc_qr_code_detect_client']
+             'scripts/rc_qr_code_detect_client',
+             'scripts/rc_boxpick_client',
+             'scripts/rc_itempick_client',
+             'scripts/rc_silhouettematch_client',
+             'scripts/rc_load_carrier_client']
 )
 
 setup(**setup_args)
